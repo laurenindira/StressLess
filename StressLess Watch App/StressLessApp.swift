aef27532014f52ee4167446c963c2e9f9044a61c
laurenindira <50187316+laurenindira@users.noreply.github.com>
2025-04-22 13:25:38 -0700

    Initial Commit

---
+++ StressLess Watch App/StressLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressLessApp.swift
+//  StressLess Watch App
+//
+//  Created by Lauren Indira on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct StressLess_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StressLess/StressLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressLessApp.swift
+//  StressLess
+//
+//  Created by Lauren Indira on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct StressLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
